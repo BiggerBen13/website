f0a743e0d2dfab7df45c92033f7d95595c36f1cf
Ben Kirschniak <ben@kirschniak.de>
2024-12-18 23:27:41 +0100

    rewrote routing

--- src/page.rs
+++ src/page.rs
@@ -1,71 +1,48 @@
 use std::error::Error;
 
+use comrak::html;
 use strum::IntoEnumIterator;
 
 use maud::{html, Markup, DOCTYPE};
 use tiny_http::Response;
 
 use crate::{
-    home, photography, util::{content, routes::Pages}
+    home, photography,
+    util::{content, routes::Pages},
 };
 
 const CSS: &str = "style/style.css";
 
 // This is the main function which handles requests
 pub fn handle_request(request: tiny_http::Request) -> Result<(), Box<dyn Error>> {
-    match Pages::get_page(request.url()) {
-        // Request to one of the valid pages
-        Ok(p) => {
-            let page: String = p.build_page()?.into_string();
-            let response = Response::from_string(page);
-            let response = response.with_header(
-                "Content-Type: text/html"
-                    .parse::<tiny_http::Header>()
-                    .unwrap(),
-            );
-            request.respond(response)?;
-            return Ok(());
-        }
-
-        // Request to either content or nothing
-        Err(_) => {
-            match content::serve_function(&request) {
-                Ok(r) => {
-                    request.respond(r)?;
-                }
-                Err(_) => {
-                    request.respond(tiny_http::Response::empty(tiny_http::StatusCode(405)))?;
-                }
-            };
-        }
-    };
+    Pages::parse_route(request.url());
     Ok(())
 }
 
-impl Pages {
-    // Contructs a page from the given page enum
-    fn build_page(&self) -> Result<Markup, Box<dyn Error>> {
-        let embed_page = match self {
-            Pages::Home => home::homepage,
-            //Pages::Blog => blog::blog_page,
-            Pages::Photography => photography::photography_page,
-            Pages::Github => || {html!{ meta http-equiv="Refresh" content="0, URL=https://github.com/BiggerBen13";}},
-            //_ => todo!(),
-        };
+// Contructs a page from the given page enum
+fn build_page(page: Pages) -> Result<Markup, Box<dyn Error>> {
+    //let embed_page = match page {
+        //Pages::Home => home::homepage,
+        ////Pages::Blog => blog::blog_page,
+        //Pages::Photography => photography::photography_page,
+        //Pages::Github => || {
+        //    html! { meta http-equiv="Refresh" content="0, URL=https://github.com/BiggerBen13";}
+        //},
+        //_ => todo!(),
+    //};
 
-        let markup = html! {
-            (DOCTYPE)
-            title {(format!("BiggerBen: {}", self))}
-            link rel="stylesheet" type="text/css" href=(CSS);
-            body {
-                div class="term" {
-                    (generate_navbar(self))
-                    (embed_page())
-                }
-            }
-        };
-        Ok(markup)
-    }
+    //let markup = html! {
+    //    (DOCTYPE)
+    //    title {(format!("BiggerBen: {}", page))}
+    //    link rel="stylesheet" type="text/css" href=(CSS);
+    //    body {
+    //        div class="term" {
+    //            (generate_navbar(&page))
+    //            (embed_page())
+    //        }
+    //    }
+    //};
+    Ok(html!(div { "hello world" }))
 }
 
 // Generates the NavBar
@@ -78,4 +55,3 @@ fn generate_navbar(current_page: &Pages) -> Markup {
         }
     }
 }
-
--- src/util/routes.rs
+++ src/util/routes.rs
@@ -25,7 +25,7 @@ impl Error for PageErrors {
 #[derive(Debug, EnumIter, PartialEq, Eq)]
 pub enum Pages {
     Home,
-    //Blog,
+    Blog(String),
     Photography,
     Github,
 }
@@ -34,9 +34,36 @@ impl Pages {
     pub fn get_route(&self) -> &str {
         match self {
             Pages::Home => "/",
-            //Pages::Blog => "/blog",
+            Pages::Blog(_) => "/blog",
             Pages::Photography => "/photography",
             Pages::Github => "/github",
+        }
+    }
+
+    pub fn parse_route(url: &str) -> Result<Self, PageErrors>{
+        println!("{url}");
+        let segments = url.split("/").collect::<Box<[&str]>>();
+        match *segments {
+            ["", ""] => return Ok(Pages::Home),
+            ["", route] => Ok(Self::parse_1_route(route)?),
+            ["", route, subroute] => Ok(Self::parse_2_route(route, subroute)?),
+
+            _ => Err(PageErrors::PageNotFound("Route unavailable!".to_string())),
+        }
+    }
+
+    fn parse_1_route(route: &str) -> Result<Self, PageErrors> {
+        match route {
+            "photography" => Ok(Self::Photography),
+            "github" | "gh" => Ok(Self::Github),
+            e => Err(PageErrors::PageNotFound(e.to_string())),
+        }
+    }
+
+    fn parse_2_route(route: &str, subroute: &str) -> Result<Self, PageErrors> {
+        match route {
+            "blog" => Ok(Self::Blog(subroute.to_string())),
+            _ => Err(PageErrors::PageNotFound(format!("{route}/{subroute}")))
         }
     }
 
@@ -60,7 +87,7 @@ impl Display for Pages {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Pages::Home => write!(f, "Home"),
-            //Pages::Blog => write!(f, "Blog"),
+            Pages::Blog(p) => write!(f, "Blog {}", p),
             Pages::Photography => write!(f, "Photography"),
             Pages::Github => write!(f, "Github"),
         }
@@ -75,13 +102,13 @@ fn test_pages_fmt() {
     assert_eq!(Pages::Github.to_string(), "Github");
 }
 
-#[test]
-fn test_get_route() {
-    assert_eq!(Pages::Home.get_route(), "/");
-    //assert_eq!(Pages::Blog.get_route(), "/blog");
-    assert_eq!(Pages::Photography.get_route(), "/photography");
-    assert_eq!(Pages::Github.get_route(), "/github")
-}
+//#[test]
+//fn test_get_route() {
+//    assert_eq!(Pages::Home.get_route(), "/");
+//    //assert_eq!(Pages::Blog.get_route(), "/blog");
+//    assert_eq!(Pages::Photography.get_route(), "/photography");
+//    assert_eq!(Pages::Github.get_route(), "/github")
+//}
 
 #[test]
 fn test_get_page() {
